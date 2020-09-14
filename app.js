ad702e95d5f17ef66963c347e6cbc5f72da661b0
Németh Zsófia <nemeth.zsofia1001@gmail.com>
2020-09-14 19:24:02 +0200

    Basic structure and user table

---
+++ app.js
@@ -0,0 +1,12 @@
+
+//const express = require("express");
+//const bodyParser = require("body-parser");
+//const mongoose = require("mongoose");
+
+//import User from "./DB/user";
+import test, {test2} from "./DB/user.js";
+
+console.log(test());
+console.log(test2());
+
+//mongoose.connect('mongodb://localhost:27017/WeddingDB',{useUnifiedTopology: true, useNewUrlParser: true});
