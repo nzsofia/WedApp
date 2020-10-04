95f68859444fa55338ae0b7df6eb09239cbfc9d3
vagle <vagilevente@gmail.com>
2020-10-04 18:29:16 +0200

    Music track list handling : adding new track from frontend side

--- client/src/components/pages/music/Music.js
+++ client/src/components/pages/music/Music.js
@@ -4,6 +4,19 @@ import './Music.css';
 function Music() {
 
   const [tracks, setTracks] = useState({list: []});
+  const [newTrack, setNewTrack] = useState({
+    artist: "",
+    title: ""
+  });
+
+  function changeTrackInput(event) {
+    const prop = event.target.id;
+    const value = event.target.value;
+    setNewTrack({
+      ...newTrack,
+      [prop]: value
+    });
+  }
 
   function getTrackList() {
     fetch("http://localhost:9000/music")
@@ -16,22 +29,19 @@ function Music() {
   }
 
   function addTrack(event) {
-    const track = {
-      artist: "Lady Gaga & Bradley Cooper",
-      title: "Shallow"
-    };
     const requestOptions = {
       method: "POST",
       headers: {
         "Accept": "application/json",
         "Content-Type": "application/json"
       },
-      body: JSON.stringify(track)
+      body: JSON.stringify(newTrack)
     };
     fetch("http://localhost:9000/music", requestOptions)
       .then(res => res.text())
       .then(res => {
         console.log(res);
+        setNewTrack({artist: "", title: ""});
         getTrackList();
       })
       .catch(err => err);
@@ -61,25 +71,38 @@ function Music() {
   useEffect(getTrackList, []);
 
   return (
-    <div className="track-list-container">
-      <ul>
-        {tracks.list.sort((a, b) => {
-          return b.users.length - a.users.length;
-        }).map((track) =>
-          <li key={track._id}>
-            {track.artist + " - " + track.title}
-            <span>
-              <input type="checkbox"
-                     id={"track-checkbox" + track._id}
-                     onChange={(e) => changeLikeOnTrack(e, track._id)}
-                     checked={track.users.includes("5f69c45607cd5b2b205799e0")}
-              />
-              <label htmlFor={"track-checkbox" + track._id}>{track.users.length}</label>
-            </span>
-          </li>
-        )}
-      </ul>
-      <button onClick={addTrack}>Add</button>
+    <div>
+      <h4>Please, add only the tracks which aren't on the list already!</h4>
+      <div className="track-list-container">
+        <form>
+          <label htmlFor="artist">Artist</label>
+          <input type="text" id="artist" onChange={changeTrackInput} value={newTrack.artist}/>
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" onChange={changeTrackInput} value={newTrack.title}/>
+          <button onClick={addTrack}>Add</button>
+        </form>
+        <ul>
+          {tracks.list.sort((a, b) => {
+            if (b.users.length - a.users.length !== 0)
+              return b.users.length - a.users.length;
+            if (b.artist !== a.artist)
+              return b.artist < a.artist ? 1 : -1;
+            return b.title < a.title ? 1 : -1;
+          }).map((track) =>
+            <li key={track._id}>
+              {track.artist + " - " + track.title}
+              <span>
+                <input type="checkbox"
+                       id={"track-checkbox" + track._id}
+                       onChange={(e) => changeLikeOnTrack(e, track._id)}
+                       checked={track.users.includes("5f69c45607cd5b2b205799e0") /* TODO get authenticated user's id */}
+                />
+                <label htmlFor={"track-checkbox" + track._id}>{track.users.length}</label>
+              </span>
+            </li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
