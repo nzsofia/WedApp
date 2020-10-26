82cb80a3aa11b1d82ca3b5ee1c4d567cff189b93
vagle <vagilevente@gmail.com>
2020-10-26 17:10:57 +0100

    Navigation bar : Material UI add on

--- client/src/components/shared/navigation-bar/NavigationBar.js
+++ client/src/components/shared/navigation-bar/NavigationBar.js
@@ -1,20 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 import './NavigationBar.scss';
 import { useHistory } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import { ExitToApp } from "@material-ui/icons";
 
 function NavigationBar() {
 
   const history = useHistory();
-  const [checked, setChecked] = useState(false);
-
-  function toggleMenu() {
-    setChecked(false);
-  }
-
-  function clickMenu() {
-    setChecked(!checked);
-  }
 
   function logout(event) {
     fetch("http://localhost:9000/logout", {
@@ -28,10 +23,10 @@ function NavigationBar() {
       .then(res => res.json())
       .then(res => {
         //if authentication failed or logout succeeded redirect to login page
-        if (res.message.code === 401 || res.message.code === 200){
+        if (res.message.code === 401 || res.message.code === 200) {
           history.push("/login");
         }
-        else{
+        else {
           event.preventDefault();
         }
       })
@@ -40,23 +35,21 @@ function NavigationBar() {
 
   return (
     <div className="nav-bar">
-      <div className="nav-bar__navigation">
-        <input type="checkbox" className="nav-bar__checkbox" id="nav-bar__checkbox" checked={checked} onChange={clickMenu} />
-        <label htmlFor="nav-bar__checkbox" className="nav-bar__navigation__box">
-          <span>&nbsp;</span>
-        </label>
-        <div className="nav-bar__background">&nbsp;</div>
-        <div className="nav-bar__container">
-          <Link className="nav-bar__button" onClick={toggleMenu} to="/">Home</Link>
-          <Link className="nav-bar__button" onClick={toggleMenu} to="/guests">Guests</Link>
-          <Link className="nav-bar__button" onClick={toggleMenu} to="/gifts">Gifts</Link>
-          <Link className="nav-bar__button" onClick={toggleMenu} to="/menu">Menu</Link>
-          <Link className="nav-bar__button" onClick={toggleMenu} to="/music">Music</Link>
-          <Link className="nav-bar__button" onClick={toggleMenu} to="/login">Login</Link>
-          <Link className="nav-bar__button" onClick={toggleMenu} to="/register">Register</Link>
-          <button className="nav-bar__button" onClick={logout} >Logout</button>
-        </div>
-      </div>
+      <AppBar position="static" color="transparent">
+        <Toolbar>
+          <div className="nav-bar__navigation">
+            <Link className="nav-bar__button" to="/">Home</Link>
+            <Link className="nav-bar__button" to="/guests">Guests</Link>
+            <Link className="nav-bar__button" to="/gifts">Gifts</Link>
+            <Link className="nav-bar__button" to="/menu">Menu</Link>
+            <Link className="nav-bar__button" to="/music">Music</Link>
+          </div>
+          <div className="spacing"></div>
+          <IconButton onClick={logout} style={{ color: "currentcolor" }}>
+            <ExitToApp />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
