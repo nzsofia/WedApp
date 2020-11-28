75a3fd9af85acaf400aab44a646cb1db70b72182
Németh Zsófia <nemeth.zsofia1001@gmail.com>
2020-11-28 21:59:41 +0100

    Add register form validation

--- client/src/components/pages/sign/register/Register.js
+++ client/src/components/pages/sign/register/Register.js
@@ -3,16 +3,25 @@ import './Register.scss';
 import {Container, Avatar, Button, CssBaseline, TextField } from "@material-ui/core";
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { useHistory } from "react-router-dom";
+import * as EmailValidator from "email-validator";
 
 function Register(props) {
   const history = useHistory();
   const { value, index, ...other } = props;
 
+  //STATES
   const [user,setUser] = useState({
     fName: "",
     lName: "",
     username: "",
     password: ""
+  });
+
+  const [formError,setFormError] = useState({
+    fName: null,
+    lName: null,
+    username: null,
+    password: null
   });
 
   const [returnMessage,setReturnMessage] = useState({
@@ -31,7 +40,45 @@ function Register(props) {
     });
   }
 
+  function validation(){
+
+    let newError = {};
+
+    if(!user.fName){
+      newError.fName = "Required";
+    }
+
+    if(!user.lName){
+      newError.lName = "Required";
+    }
+
+    if(!user.username){
+      newError.username = "Required";
+    }
+    else if (!EmailValidator.validate(user.username)) {
+      newError.username = "Invalid email address";
+    }
+
+    if(!user.password){
+      newError.password = "Required";
+    }
+    else if (user.password.length < 3) {
+      newError.password = "Password must be at least 3 characters long";
+    }
+
+    setFormError(newError);
+    //check if there were any errors
+    if(Object.keys(newError).length === 0) return true;
+    return false;
+  }
+
   function performRegistration(event) {
+    //validate form fields
+    if(!validation()){
+      event.preventDefault();
+      return;
+    }
+
     // check in database if user already exist
     const requestOptions = {
       method: "POST",
@@ -85,6 +132,8 @@ function Register(props) {
               autoFocus
               onChange={handleChange}
               value={user.fName}
+              error={formError.fName}
+              helperText={formError.fName}
             />
             <TextField
               variant="outlined"
@@ -95,6 +144,8 @@ function Register(props) {
               name="lName"
               onChange={handleChange}
               value={user.lName}
+              error={formError.lName}
+              helperText={formError.lName}
             />
             <TextField
               variant="outlined"
@@ -107,6 +158,8 @@ function Register(props) {
               autoComplete="email"
               onChange={handleChange}
               value={user.username}
+              error={formError.username}
+              helperText={formError.username}
             />
             <TextField
               variant="outlined"
@@ -119,6 +172,8 @@ function Register(props) {
               autoComplete="current-password"
               onChange={handleChange}
               value={user.password}
+              error={formError.password}
+              helperText={formError.password}
             />
             <Button
               type="button"
