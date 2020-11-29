b50b77b7b63244def9b80823342ffcc9fadd97c4
vagle <vagilevente@gmail.com>
2020-11-29 01:40:40 +0100

    Menu content and design

--- client/src/components/pages/menu/Menu.js
+++ client/src/components/pages/menu/Menu.js
@@ -3,6 +3,7 @@ import './Menu.scss';
 import { useHistory } from "react-router-dom";
 import NavigationBar from "../../shared/navigation-bar/NavigationBar";
 import * as request from "../../../services/request";
+import box from "../../../assets/svg/compositions/boxes/floral-box-rec-port-2.svg";
 
 function Menu() {
   const history = useHistory();
@@ -24,7 +25,28 @@ function Menu() {
   return (
     <div>
       <NavigationBar />
-      <div>Menu works!</div>
+      <div className="menu-container">
+        <img src={box} alt="Menu container" />
+        <div className="menu">
+          <h1 className="menu__title">Wedding Menu</h1>
+          <div className="menu__dish">
+            <h2 className="menu__dish__title">Starter</h2>
+            <p className="menu__dish__description">Curry Roasted Parsnip Soup with Coriander and Almonds</p>
+            <p className="menu__dish__description">Spring Green Salad</p>
+          </div>
+          <div className="menu__dish">
+            <h2 className="menu__dish__title">Main dishes</h2>
+            <p className="menu__dish__description">Barbecue Favorites: Fried Chicken, Mac and Cheese and Biscuits</p>
+            <p className="menu__dish__description">Grilled Chicken and Cherry Tomato Skewers</p>
+            <p className="menu__dish__description">Butternut Squash, Fresh Fruit and Stake</p>
+          </div>
+          <div className="menu__dish">
+            <h2 className="menu__dish__title">Dessert</h2>
+            <p className="menu__dish__description">S'mores Chocolate-Covered Marshmallows</p>
+            <p className="menu__dish__description">Mousse Cups</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
